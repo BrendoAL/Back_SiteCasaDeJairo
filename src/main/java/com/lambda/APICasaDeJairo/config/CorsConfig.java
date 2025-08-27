8034dafcdf33ec4075a3ddb53de310d6b977d86c
Brendo Almeida <121973401+BrendoAL@users.noreply.github.com>
2025-08-27 10:22:37 -0300

    Update CorsConfig.java
     Trocando descrição
    
    
    Co-authored-by: rafael2297 <rafael22celio@gmail.com>

--- src/main/java/com/lambda/APICasaDeJairo/config/CorsConfig.java
+++ src/main/java/com/lambda/APICasaDeJairo/config/CorsConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-//configuracao do acesso do front ao back
+//configuracao do acesso do Frontend ao Backend
 @Configuration
 public class CorsConfig {
     @Bean
